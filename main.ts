1b46c78d8f2496adb3d5c7deef890fbd91c7f83d
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-09-17 11:07:25 -0400

    changed the program to calculate area and perimeter of a rectangle

--- main.ts
+++ main.ts
@@ -2,7 +2,17 @@
  *
  * Created by: Francisco
  * Created on: Sep 2024
- * This program says Hello, World!
+ * This program shows the area and perimeter of a 5 by 3 rectangle
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+//This calculates the rectangle's area
+basic.showString('Area = ' + (5 * 3).toString())
+
+basic.clearScreen()
+basic.pause(1000)
+
+//This calculated the rectangle's perimeter
+basic.showString('Perimeter = ' + (5 * 2 + 3 * 2).toString())
